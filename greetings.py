00a66ee766767fe1eaedb8b515f180c3e993a206
Ekaterina Makarova <namekatiaroma@gmail.com>
2024-12-13 17:53:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = "namekatia"
+title = 'В ожидании Нового года'
+content = ('Подарки близким от Деда мороза')
+status = 'В процессе'
+created_date = "13-12-2024"
+issue_date = "31-12-2024"
+
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status )
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
